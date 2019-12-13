ba707d3e452f51ed00f942b4ee164a9eec175645
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2019-12-13 00:51:20 -0500

    Updates

---
+++ precache-manifest.69a17ca5464c6d93bdaf00e4150c10c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535238fbaae7565e9d44bab67cc9f164",
+    "url": "/ml-presentation/index.html"
+  },
+  {
+    "revision": "ae822e173a3661649a1c",
+    "url": "/ml-presentation/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "382df04bac16449eee68",
+    "url": "/ml-presentation/static/js/2.74a2d695.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/ml-presentation/static/js/2.74a2d695.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae822e173a3661649a1c",
+    "url": "/ml-presentation/static/js/main.50f1c44d.chunk.js"
+  },
+  {
+    "revision": "c4f1889e5800941700e5",
+    "url": "/ml-presentation/static/js/runtime-main.8481771b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ml-presentation/static/media/logo.5d5d9eef.svg"
+  }
+]);
